97a9c202fa9e6f6662c96e41f972d6179866f67e
Marina <marinaandjelkovic456@gmail.com>
2019-03-25 19:28:44 +0100

    dva inputa, fokus ide na 2.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app" class="container">
-    <h1>prvi input sa focus directive</h1>
-    <single-input-component></single-input-component>
+    <!-- <h1>prvi input sa focus directive</h1>
+    <single-input-component></single-input-component> -->
+    <h1>dva inputa sa focus directive</h1>
+    <two-inputs-component></two-inputs-component>
   </div>
 </template>
 
 <script>
 import SingleInputComponent from "./components/SingleInputComponent.vue";
+import TwoInputsComponent from "./components/TwoInputsComponent.vue";
 
 export default {
   name: "app",
   components: {
-    SingleInputComponent
+    SingleInputComponent,
+    TwoInputsComponent
   }
 };
 </script>
---
+++ src/components/TwoInputsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="mb-3">
+            <b-form-input v-focus />
+        </div>
+
+        <div class="mt-3">
+            <b-form-input v-focus />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TwoInputsComponent"
+};
+</script>
+
+<style scoped>
+</style>
