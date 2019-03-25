a0f18ff232eff10fbc4f6b9bd7d859a1710b24a7
Marina <marinaandjelkovic456@gmail.com>
2019-03-25 21:47:40 +0100

    plugin 2 - ispisivanje boja, upgrade

--- src/components/MyButtonsComponent.vue
+++ src/components/MyButtonsComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <!-- <button :style="{ 'background-color': buttonColor }" @click="generateButton">Generate random color</button> -->
         <button :style="{ backgroundColor: buttonColor }" @click="generateButton">Generate random color</button>
-        <button @click="generate10Colors">List 10 random colors</button>
+        <button @click="generate10Colors(5)">List 10 random colors</button>
         <div v-for="(color, index) in colorsDiv" :key="index" :style="{ backgroundColor: color }">{{ color }}</div>
     </div>
 </template>
@@ -22,10 +22,8 @@ export default {
     generateButton() {
       this.buttonColor = GradientGenerator.generateColorHex();
     }, 
-    generate10Colors() {
-      console.log(this.colorsDiv);
-      this.colorsDiv = GradientGenerator.generate10Colors();
-      console.log(this.colorsDiv);
+    generate10Colors(numOfColors) {
+      this.colorsDiv = GradientGenerator.generate10Colors(numOfColors);
     }
   }
 };
--- src/helpers/GradientGenerator.js
+++ src/helpers/GradientGenerator.js
@@ -2,9 +2,9 @@ class GradientGenerator {
   generateColorHex() {
     return '#' + ('000000' + Math.random().toString(16).slice(2, 8)).slice(-6);
   };
-  generate10Colors() {
+  generate10Colors(numOfColors) {
     let colorList = [];
-    for (let i = 0; i<10; i++) {
+    for (let i = 0; i < numOfColors; i++) {
       colorList.push(this.generateColorHex());
     }
     return colorList;
